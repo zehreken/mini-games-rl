8c9017bfdb4ad19e833217c6c990bac2a63468bb
zehreken <zehreken@gmail.com>
2026-03-04 20:57:21 +0100

    Add initial files

---
+++ Source/MiniGamesRL/MiniGamesRL.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiniGamesRL.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiniGamesRL, "MiniGamesRL" );
